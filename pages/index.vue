1e467ea589d1dc686b9f9642d7ddad5781c03f0a
Amitabh Kumar <157296234+kamitabh2003@users.noreply.github.com>
2024-05-23 14:07:47 +0530

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-row dense>
+      <Video
+        :video="category.video"
+        :poster="category.videoPoster"
+      >
+      </Video>
+      <Category />
+      <Gallery />
+      <Team />
+    </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      category: {
+        name: 'Carré potager',
+        videoPoster: 'video_preview.webp',
+        video: 'flower.webm'
+      }
+    }
+  }
+}
+</script>
